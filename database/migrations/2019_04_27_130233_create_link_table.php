6dde9a556f2c804cc1970aa9c97dcf235db05bba
ipskm <ipskm@outlook.com>
2019-04-28 10:31:05 +0000

    First commit

---
+++ database/migrations/2019_04_27_130233_create_link_table.php
@@ -0,0 +1,36 @@
+
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('record', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->string('title');
+	    $table->text('decription');
+	    $table->float('amount',10,2);
+	    $table->enum('list',['revenue','expenditure'])->defult('revenue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('record');
+    }
+}
